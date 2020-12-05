e41f4643e81a133e44f97fd0dc46b0f903b79121
Jonathan Bouchard <jonathaneutron@hotmail.com>
2020-12-05 07:45:26 +0000

    Improved Day 4

--- d4/solve.py
+++ d4/solve.py
@@ -1,64 +1,33 @@
 from os.path import dirname, join
-import re
+from re import match
 
 current_dir = dirname(__file__)
 file_path = join(current_dir, "input/data.txt")
 file = open(file_path, "r")
 
-passports = []
-required_fields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
-colors = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
-valid_passports_p1 = 0
-valid_passports_p2 = 0
+passports = file.read().split("\n\n")
+rules = {
+    "byr": lambda value: 1920 <= int(value) <= 2002,
+    "iyr": lambda value: 2010 <= int(value) <= 2020,
+    "eyr": lambda value: 2020 <= int(value) <= 2030,
+    "hgt": lambda value: bool(
+        match("^(1[5-8][0-9]|19[0-3])(cm)$|^(59|6[0-9]|7[0-6])(in)$", value)
+    ),
+    "hcl": lambda value: bool(match("^#[a-zA-Z0-9]{6}$", value)),
+    "ecl": lambda value: value in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"],
+    "pid": lambda value: len(value) == 9 and value.isnumeric(),
+    "cid": lambda value: True,
+}
 
-
-def in_range(value, lbound, rbound):
-    value = int(value)
-    return lbound <= value <= rbound
-
-
-def valid_height(value):
-    if "cm" in value:
-        value = int(value.replace("cm", ""))
-        if not in_range(value, 150, 193):
-            return False
-    elif "in" in value:
-        value = int(value.replace("in", ""))
-        if not in_range(value, 59, 76):
-            return False
-    else:
-        return False
-    return True
-
-
-def validate(name, value):
-    if name == "byr" and not in_range(value, 1920, 2002):
-        return False
-    elif name == "iyr" and not in_range(value, 2010, 2020):
-        return False
-    elif name == "eyr" and not in_range(value, 2020, 2030):
-        return False
-    elif name == "hgt" and not valid_height(value):
-        return False
-    elif name == "hcl" and not re.match("^\\#[a-zA-Z0-9]{6}$", value):
-        return False
-    elif name == "ecl" and not any(color in value for color in colors):
-        return False
-    elif name == "pid" and len(value) != 9 and not isinstance(value, int):
-        return False
-    elif name == "cid":
-        pass
-    return True
-
-
-for line in file.read().split("\n\n"):
-    passports.append(line)
-
+valid_passports_p1 = valid_passports_p2 = 0
 for passport in passports:
-    if all(required_field in passport for required_field in required_fields):
+    if all(
+        required_field in passport
+        for required_field in {k: rules[k] for k in rules.keys() - {"cid"}}
+    ):
         valid_passports_p1 += 1
-        fields = passport.split()
-        if all(validate(*field.split(":")) for field in fields):
+        fields = list(x.split(":") for x in passport.split())
+        if all(list(rules[name](value) for name, value in fields)):
             valid_passports_p2 += 1
 
 # Part 1
